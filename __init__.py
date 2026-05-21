412590db288a427e446926c6c5a8e9bcce0306c8
xflame-lgtm <x.delacerna.535556@umindanao.edu.ph>
2026-05-21 17:35:03 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-# pages/__init__.py
-from .dashboard import DashboardPage
-from .products  import ProductsPage
-from .stock_in  import StockInPage
-from .suppliers import SuppliersPage
-from .low_stock import LowStockPage
