8a5d07f458828bb16d59961209d3743504d6fe4b
xflame-lgtm <x.delacerna.535556@umindanao.edu.ph>
2026-05-23 16:48:47 +0800

    Add files via upload

---
+++ sari_sari/pages/__init__.py
@@ -0,0 +1,6 @@
+# pages/__init__.py
+from .dashboard import DashboardPage
+from .products  import ProductsPage
+from .stock_in  import StockInPage
+from .suppliers import SuppliersPage
+from .low_stock import LowStockPage
